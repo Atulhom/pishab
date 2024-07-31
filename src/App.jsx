4fbd0850a89c02e5996a40e06ca8c0e6cc639672
juan guasch <joanguaschduran@gmail.com>
2024-07-31 05:28:42 +0200

    React routes dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import React from "react"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './pages/Header.jsx';
+import Search from './pages/Search.jsx';
+import LatestRooms from './pages/LatestRooms.jsx';
+import Citycardold from './pages/Citycardold.jsx';
+import Questions from './pages/Questions.jsx';
+import TopRooms from './pages/TopRooms.jsx';
+import Footer from './pages/Footer.jsx';
 
-
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/latest-rooms" element={<LatestRooms />} />
+          <Route path="/top-rooms" element={<TopRooms />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/citycard" element={<Citycardold />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-//import Card from './features/Card.jsx'
-import Header from './features/Header.jsx'
-import Search from './features/Search.jsx'
-import LatestRooms from './features/LatestRooms.jsx'
-import Citycardold from './features/Citycardold.jsx'
-import Questions from './features/Questions.jsx'
-import TopRooms from './features/TopRooms.jsx'
-import Footer from './features/Footer.jsx'
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Header />
-    <Search />
-    <LatestRooms />
-    <TopRooms />
-    <Questions />
-    <br />
-    <Citycardold />
-    <Footer />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
